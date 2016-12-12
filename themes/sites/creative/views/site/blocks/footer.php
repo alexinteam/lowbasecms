2d3308a56650295c00700605b34f898f09bd5852
Anton Shelyganov <antonshell@yandex.ru>
2016-12-12 14:56:57 +0300

    implemented sites blocks templates

---
+++ themes/sites/creative/views/site/blocks/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <section id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Footer</h2>
+                </div>
+            </div>
+        </div>
+    </section>
+</footer>
