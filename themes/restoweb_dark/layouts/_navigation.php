31579dcf15d56022b104f666fb0b10aaee6fcdad
Anton Shelyganov <antonshell@yandex.ru>
2016-12-20 16:05:13 +0300

    changes in main site template

--- themes/restoweb_dark/layouts/_navigation.php
+++ themes/restoweb_dark/layouts/_navigation.php
@@ -11,14 +11,15 @@
         </div>
         <div id="navbar" class="collapse navbar-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="" class="main-link">Возможности</a></li>
-                <li><a href="cost.html">Тарифные планы</a></li>
-                <li><a href="javascript:" data-toggle="modal" data-target="#reg">Регистрация</a></li>
-                <li><a href="/admin">Войти в кабинет</a></li>
+                <li><a href="#block-opportunities" class="main-link">Возможности</a></li>
+                <li><a href="#block-pricing">Тарифные планы</a></li>
+                <li><a href="/signup">Регистрация</a></li>
+                <!--<li><a href="javascript:" data-toggle="modal" data-target="#reg">Регистрация</a></li>-->
+                <li><a href="/login">Войти в кабинет</a></li>
             </ul>
             <div class="block-right pull-right">
                 <p class="phone-rest">+7 (495) 799-09-62</p>
-                <a href="javascript:" class="login" data-toggle="modal" data-target="#support">Обратная связь</a>
+                <a href="#block-contact" class="login" data-toggle="modal" data-target="#support">Обратная связь</a>
             </div>
         </div><!--/.nav-collapse -->
     </div>
--- views/site/index.php
+++ views/site/index.php
@@ -6,7 +6,7 @@
             <p class="description-after">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
         </div>
     </div>
-    <div class="page2">
+    <div id="block-opportunities" class="page2">
         <div class="block-country">
             <div class="table-country table-left">
                 <div class="cell-country anim-right">
@@ -59,7 +59,7 @@
             <p class="under">тестовый период составляет 7 дней</p>
         </div>
     </div>
-    <div class="page6">
+    <div id="block-pricing" class="page6">
         <div class="w-main">
             <p class="h2-title anim">ТАРИФНЫЕ ОПЦИИ КОМПАНИИ</p>
             <p class="description-after anim">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
@@ -121,7 +121,7 @@
             </div>
         </div>
     </div>
-    <div class="page5">
+    <div id="block-contact" class="page5">
         <div class="w-main anim">
             <p class="h2-title">У ВАС ИМЕЮТСЯ ВОПРОСЫ?</p>
             <p class="description-after support-text">Мы с радостью ответим на каждый Ваш вопрос в течение одного рабочего дня, <br>для того чтобы связаться с нами - нажмите кнопку ниже</p>
